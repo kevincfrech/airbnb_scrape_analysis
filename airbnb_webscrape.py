7da57f0236775c7e40909f1e03d047156f56682c
kevincfrech <kevincfrech@gmail.com>
2020-12-20 19:56:43 -0400

    initial commit

---
+++ airbnb_webscrape.py
@@ -0,0 +1,237 @@
+import sys
+import time
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup as bs
+
+def Airbnb_webscrape(city_name, state, max_pages = 14):
+    '''Defines css class identifiers to be used in spider and webscraper'''
+    airbnb_url = 'https://airbnb.com'
+    next_page_class = '_za9j7e',
+    #features to be scraped
+    title_class = '_bzh5lkq',
+    type_location_class = '_167qordg',
+    price_class = '_l2ulkt8',
+    rooms_class = '_kqh46o'
+    listing_links_class = '_gjfol0'
+    forbidden_characters = [',', '.', '✪', '|', '/', '"', '*', '-', '+',
+                          '✤', '♥', '~', '☆', '🎓','🌟','🚑','⚡', '🌈'
+                          '★','☀','❤','✔', '🏳️', '★']
+
+
+
+    def city_url(city_name, state, country='United-States'):
+        '''build url for city and check in and out dates'''
+        base = 'https://www.airbnb.com/s/'
+        post = '/homes?tab_id=home_tab&refinement_paths'
+        url = base + city_name + '--' + state + '--' + country + post
+        return str(url)
+
+    def get_page_links(url):
+        '''get links for pages of listings, max number is 15 pages'''
+
+        # add first page to list
+        page_links = []
+        page_links.append(url)
+        link = url
+
+        # add pages 2-15 to list
+        for i in range(max_pages):
+            try:
+            # make soup and extraxt next page link
+                page = requests.get(link)
+                soup = bs(page.content, 'html.parser')
+                link = soup.find(class_=next_page_class).get('href')
+                link = airbnb_url + link
+                page_links.append(link)
+                print(link)
+            except:
+                print('page limit reached')
+                break
+        print(len(page_links))
+        return page_links
+
+    def scrape_listings_from_page(url):
+        '''scrapes multi listing page, only extracts basic info'''
+        # make soup
+        page = requests.get(url)
+        soup = bs(page.content, 'html.parser')
+
+        # scrape titles and remove special characters
+        titles = []
+        for i in soup.find_all(class_=title_class):
+            t = i.get_text()
+            for character in forbidden_characters:
+                t = t.replace(character, ' ')
+            titles.append(t)
+
+        # scrape dwelling type and location, split text and append into 2 separate lists
+        dwelling_types = []
+        locations = []
+        for i in soup.find_all(class_=type_location_class):
+            t = i.get_text()
+            if 'reviews' not in t:
+                split = t.find(' in ')
+                dwelling_type = t[:split]
+                dwelling_types.append(dwelling_type)
+                location = t[split:].replace('in ', '')
+                locations.append(location)
+
+        # scrape prices, leave only numbers
+        prices = []
+        for i in soup.find_all(class_=price_class):
+            t = i.get_text()
+            t = t.replace('From', '')
+            t = t.replace('Price:$', '')
+            t = t.replace(' / night', '')
+            prices.append(t)
+
+        # scrape room description and featured amentities
+        rooms_amenities = []
+        for i in soup.find_all(class_=rooms_class):
+            rooms_amenities.append(i.get_text())
+
+        rooms = []
+        n = 0
+        #for i in range(int(int(len(titles)) / 2)):
+        for i in range(20):
+            rooms.append(rooms_amenities[n])
+            n += 2
+
+        # split rooms
+        guests = []
+        bedrooms = []
+        beds = []
+        bathrooms = []
+        for i in rooms:
+            words = ['guests', 'guest', 'bedrooms', 'bedroom', 'beds', 'bed', 'bath' 'baths']
+            for word in words:
+                i = i.replace(word, '')
+            t = i.split('·')
+            if len(t) == 4:
+                guests.append(t[0])
+                bedrooms.append(t[1])
+                beds.append(t[2])
+                bathrooms.append(t[3])
+            else:
+                guests.append('1')
+                bedrooms.append('1')
+                beds.append('1')
+                bathrooms.append('1')
+
+        # split amenities
+        amenities = []
+        n = 1
+        #for i in range(int(int(len(titles)) / 2)):
+        for i in range(20):
+            amenities.append(rooms_amenities[n])
+            n += 2
+
+        amenity_1 = []
+        amenity_2 = []
+        amenity_3 = []
+        amenity_4 = []
+
+        for i in amenities:
+            t = i.split('·')
+            if len(t) == 4:
+                amenity_1.append(t[0])
+                amenity_2.append(t[1])
+                amenity_3.append(t[2])
+                amenity_4.append(t[3])
+            elif len(t) == 3:
+                amenity_1.append(t[0])
+                amenity_2.append(t[1])
+                amenity_3.append(t[2])
+                amenity_4.append('None')
+            elif len(t) == 2:
+                amenity_1.append(t[0])
+                amenity_2.append(t[1])
+                amenity_3.append('None')
+                amenity_4.append('None')
+            elif len(t) == 1:
+                amenity_1.append(t[0])
+                amenity_2.append('None')
+                amenity_3.append('None')
+                amenity_4.append('None')
+            elif len(t) == 0:
+                amenity_1.append('None')
+                amenity_2.append('None')
+                amenity_3.append('None')
+                amenity_4.append('None')
+
+
+
+        # scrape individual listing urls
+        listing_links = []
+        for link in soup.find_all(class_=listing_links_class):
+            link = link.get('href')
+            link = airbnb_url + link
+            listing_links.append(link)
+
+        print(len(titles))
+        print(len(dwelling_types))
+        print(len(locations))
+        print(len(prices))
+        print(len(guests))
+        print(len(bedrooms))
+        print(len(beds))
+        print(len(bathrooms))
+        print(len(amenity_1))
+        print(len(amenity_2))
+        print(len(amenity_3))
+        print(len(amenity_4))
+        print(len(listing_links))
+
+        print(len(titles) == len(locations) == len(prices) == len(guests) == len(bedrooms) == len(bathrooms) == len(
+            amenity_1) == len(amenity_2) == len(amenity_3) == len(amenity_4) )
+
+        # Build listing list objects
+        listings = []
+        column_headers = ['title', 'type', 'location', 'price',
+                          'guests', 'bedrooms', 'beds',
+                          'bathrooms', 'amenity_1', 'amenity_2',
+                          'amenity_3', 'amenity_4', 'link']
+        # listings.append(column_headers)
+        for t, type, loc, p, g, bdr, bed, bath, a1, a2, a3, a4, li in zip(titles, dwelling_types,locations,
+                                                                          prices, guests, bedrooms,
+                                                                          beds, bathrooms, amenity_1,
+                                                                          amenity_2,amenity_3, amenity_4,
+                                                                          listing_links):
+            listing = []
+            f = [t, type, loc, p, g, bdr, bed, bath, a1, a2, a3, a4, li]
+            for i in f:
+                listing.append(i)
+
+            listings.append(listing)
+        ldf = pd.DataFrame(listings)
+        return (ldf)
+
+    def scrape_city(city_name, state, csv_output = r'/home/kfrech/project/airbnb_webscrape/csv/'):
+        try:
+            url = city_url(city_name, state)
+            page_links = get_page_links(url)
+            all_scraped_data = []
+            column_headers = ['title', 'type', 'location', 'price',
+                              'guests', 'bedrooms', 'beds',
+                              'bathrooms', 'amenity_1', 'amenity_2',
+                              'amenity_3', 'amenity_4', 'link']
+            all_scraped_data.append(column_headers)
+            sdf = pd.DataFrame(all_scraped_data)
+
+            for url in page_links:
+                ldf = scrape_listings_from_page(url)
+                sdf = sdf.append(ldf)
+
+            csv_name = city_name + '_airbnb.csv'
+            sdf.to_csv(csv_output + csv_name)
+            print(sdf)
+        finally:
+            print('webscrape complete')
+
+    scrape_city(city_name, state)
+
+#Airbnb_webscrape('Boston')
+
+if __name__ == "__main__":
+    Airbnb_webscrape(sys.argv[1], sys.argv[2], int(sys.argv[3]))
